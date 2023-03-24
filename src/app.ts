a9e005ca7dd7de00d9f5194107adfb5963237434
lwinmin-12 <lmo.lwinmin@gmail.com>
2023-03-24 11:40:11 +0630

    cors*

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ import cors from 'cors'
 const app  = express()
 app.use(express.json())
 app.use(fileUpload())
-app.use(cors({origin: 'https://lucky-kappa.vercel.app'}))
+app.use(cors({origin: '*'}))
 const server = require('http').createServer(app)
 
 
